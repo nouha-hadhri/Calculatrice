6bd50f329e278737c00573c7751148221f31c32f
Nouha Hadhri <nouha.hadhri@enis.tn>
2025-11-05 22:42:25 +0100

    Sauvegarde avant de revenir à la première version

--- Operation.java
+++ Operation.java
@@ -5,4 +5,13 @@ public class Operation {
 	System.out.println("Bonjour");        
 	return x * y;
     }
+  // Méthode statique qui additionne deux entiers
+    public static int addition(int a, int b) {
+        return a + b;
+    }
+
+    // Méthode statique qui soustrait deux entiers
+    public static int soustraction(int a, int b) {
+        return a - b;
+    }
 }
