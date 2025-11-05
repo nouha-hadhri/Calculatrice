da7ce18563db6d926390245ea8520b57539ba7ba
Nouha Hadhri <nouha.hadhri@enis.tn>
2025-11-05 23:01:44 +0100

    Ajout d’un message bienvenue

--- Main.java
+++ Main.java
@@ -3,7 +3,8 @@ public class Main {
     public static void main(String[] args) {
         int a = 2;
         int b = 3;
-
+// Affichage du message de bienvenue
+        System.out.println("Bienvenue");
         int mult = Operation.multiplication(a, b);
         int sous = Operation.soustraction(a, b);
         int add = Operation.addition(a, b);
